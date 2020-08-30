22ae9977565b88f7679d80449c650f4840f06321
Samrith Shankar <samrith.298@gmail.com>
2020-08-31 00:45:55 +0200

    improvement: upgrade project to use Typescript and add linting

---
+++ pages/_app.tsx
@@ -0,0 +1,10 @@
+import '../styles/globals.css';
+
+const MyApp: React.FC<{ Component: React.ComponentType; pageProps: any }> = ({
+    Component,
+    pageProps,
+}) => {
+    return <Component {...pageProps} />;
+};
+
+export default MyApp;
