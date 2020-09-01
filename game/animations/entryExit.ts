ed17d40ea464b67f103ba9edc7ea4e6638f5f124
Samrith Shankar <samrith.298@gmail.com>
2020-09-02 01:07:50 +0200

    chore(cleanup): remove unwanted logs

--- game/animations/entryExit.ts
+++ game/animations/entryExit.ts
@@ -5,8 +5,6 @@ export const UpInOut = EntryExit({
     opacity: [0, 1, 0],
 });
 
-console.log(UpInOut);
-
 export const DownInOut = EntryExit({
     transform: ['translateY(100%)', 'translateY(0%)', 'translateY(100%)'],
     opacity: [0, 1, 0],
