a856481a9db2b65517c2ee024858a4b2aa4870c5
Samrith Shankar <samrith.298@gmail.com>
2020-09-02 01:50:02 +0200

    chore(seo): updated open graph and twitter settings

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,9 @@
 import { NextSeo } from 'next-seo';
 
+const title = 'Samrith Shankar - Fullstack Developer';
+const description = 'Fullstack developer from Bombay (🇮🇳) working in Paris (🇫🇷).';
+const url = 'https://samrith.dev/';
+
 const Portfolio: React.FC<{ Component: React.ComponentType; pageProps: any }> = ({
     Component,
     pageProps,
@@ -7,8 +11,24 @@ const Portfolio: React.FC<{ Component: React.ComponentType; pageProps: any }> = 
     return (
         <>
             <NextSeo
-                title="Samrith Shankar"
-                description="Fullstack developer from Bombay (🇮🇳) working in Paris (🇫🇷)."
+                title={title}
+                description={description}
+                canonical={url}
+                openGraph={{
+                    url,
+                    title,
+                    description,
+                    images: [
+                        {
+                            url:
+                                'https://raw.githubusercontent.com/samrith-s/portfolio/master/public/samrith-shankar-opengraph.jpg',
+                        },
+                    ],
+                }}
+                twitter={{
+                    handle: '@tueieo',
+                    cardType: 'summary_large_image',
+                }}
             />
             <Component {...pageProps} />
         </>
