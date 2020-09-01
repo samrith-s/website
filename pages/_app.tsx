6f5471282f91b55ab43ece4abcec4adf9c52f515
Samrith Shankar <samrith.298@gmail.com>
2020-09-01 13:58:33 +0200

    feat(seo): setup next-seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
-const MyApp: React.FC<{ Component: React.ComponentType; pageProps: any }> = ({
+import { NextSeo } from 'next-seo';
+
+const Portfolio: React.FC<{ Component: React.ComponentType; pageProps: any }> = ({
     Component,
     pageProps,
 }) => {
-    return <Component {...pageProps} />;
+    return (
+        <>
+            <NextSeo
+                title="Samrith Shankar"
+                description="Fullstack developer from Bombay working in Paris."
+            />
+            <Component {...pageProps} />
+        </>
+    );
 };
 
-export default MyApp;
+export default Portfolio;
