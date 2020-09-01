6179c4279e65cf89fc255abc26d8e400a4ddcdc3
Samrith Shankar <samrith.298@gmail.com>
2020-09-02 01:06:19 +0200

    refactor(scene): set default scene on page load

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 
 import { Intro } from '../game/controllers/scenes/Intro.scene';
@@ -5,6 +7,19 @@ import { GlobalStyle } from '../styles';
 import { theme } from '../styles/theme';
 
 const Home: React.FC = () => {
+    const router = useRouter();
+    const { scene: urlScene } = router.query;
+    const [scene, setScene] = useState(urlScene);
+
+    useEffect(() => {
+        if (!scene) {
+            setScene('intro');
+            router.replace(`/?scene=intro`, `/?scene=intro`, {
+                shallow: true,
+            });
+        }
+    }, [scene, router]);
+
     return (
         <ThemeProvider theme={theme}>
             <GlobalStyle />
