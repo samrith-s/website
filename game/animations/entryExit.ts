7c5cf40f0f4c1af8308571635d83230c3d2aeeda
Samrith Shankar <samrith.298@gmail.com>
2020-09-02 01:04:49 +0200

    feat(animation): added entry exit animations and helpers

---
+++ game/animations/entryExit.ts
@@ -0,0 +1,13 @@
+import { EntryExit } from './_animationCreator';
+
+export const UpInOut = EntryExit({
+    transform: ['translateY(-100%)', 'translateY(0%)', 'translateY(-100%)'],
+    opacity: [0, 1, 0],
+});
+
+console.log(UpInOut);
+
+export const DownInOut = EntryExit({
+    transform: ['translateY(100%)', 'translateY(0%)', 'translateY(100%)'],
+    opacity: [0, 1, 0],
+});
