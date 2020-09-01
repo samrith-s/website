a87700d5b2d29bdc78e1d3d8666851cfe1bf7e47
Samrith Shankar <samrith.298@gmail.com>
2020-09-01 22:33:09 +0200

    feat(app): added Sprite component to handle spritesheets

---
+++ game/helpers/Sprite.tsx
@@ -0,0 +1,32 @@
+import styled, { keyframes, css } from 'styled-components';
+
+interface SpriteProps {
+    src: string;
+    width: number;
+    height: number;
+    frames: number;
+    rate?: number;
+    vertical?: boolean;
+}
+
+export const Sprite = styled.span.withConfig({
+    shouldForwardProps(prop, defaultValidatorFn) {
+        return !['src', 'width', 'height'].includes(prop) && defaultValidatorFn(prop);
+    },
+})<SpriteProps>`
+    ${({ src, width, height, frames, rate = 1 }) => css`
+        display: inline-block;
+        width: ${width}px;
+        height: ${height}px;
+        animation: ${(props) => moveSprite(props)} ${1 / rate}s steps(${frames}) infinite;
+        background-image: url(${src});
+    `}
+`;
+
+function moveSprite({ width, frames, vertical }: Partial<SpriteProps>) {
+    return keyframes`
+        100% {
+            background-position-${vertical ? 'y' : 'x'}: ${frames * width}px;
+        }
+    `;
+}
