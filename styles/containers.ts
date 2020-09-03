579158b9e45258eb2b25ab232ee8e18397a5da65
Samrith Shankar <samrith.298@gmail.com>
2020-09-03 14:00:24 +0200

    style(scene): added extensible SceneContainer style

---
+++ styles/containers.ts
@@ -0,0 +1,13 @@
+import { motion } from 'framer-motion';
+import styled from 'styled-components';
+
+export const SceneContainer = styled(motion.div)`
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    display: flex;
+    width: 100%;
+    height: 100%;
+`;
