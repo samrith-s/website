0a1c37856be82e89a9eee44cdb3ec7cb900dd83e
Samrith Shankar <samrith.298@gmail.com>
2020-09-04 02:13:33 +0200

    refactor(app): updated Backdrop layout to play with animations

--- game/assets/Backdrop.tsx
+++ game/assets/Backdrop.tsx
@@ -1,9 +1,13 @@
-import { AnimatePresence } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useContext, useState } from 'react';
 import styled from 'styled-components';
 
 import CloudsSprite from '../../public/sprites/clouds.png';
-import { lighten, padding } from '../../styles/helpers';
+import { lighten, padding, color } from '../../styles/helpers';
+import { GameContext, ScrollContext } from '../context';
+import { Player } from '../controllers/Player';
 import { Scroller } from '../helpers/Scroller';
+import { SCENES } from '../variables';
 
 const Container = styled.div`
     position: relative;
@@ -17,18 +21,35 @@ const Container = styled.div`
 `;
 
 const ScrollerContainer = styled.div`
+    position: relative;
     z-index: 1;
     width: 100%;
     height: 100%;
 `;
 
 const Scene = styled.div`
+    position: relative;
+    z-index: 2;
+    display: flex;
+    width: 100%;
+    height: calc(100% + 50px);
+    overflow-y: hidden;
+    ${padding(30, 0, 5)}
+`;
+
+const SceneContent = styled(motion.div)`
+    position: relative;
+    min-width: 100vw;
+    height: 100%;
+`;
+
+const SceneScrollHide = styled.div`
     position: absolute;
-    top: 0;
     right: 0;
-    bottom: 20vh;
+    bottom: 0;
     left: 0;
-    margin-top: 300px;
+    height: 50px;
+    background: ${color('base')};
 `;
 
 const Ground = styled.div`
@@ -45,16 +66,29 @@ const Ground = styled.div`
     font-size: 0.8rem;
 `;
 
-export const Backdrop: React.FC = ({ children }) => (
-    <Container>
-        <ScrollerContainer>
-            <Scroller src={CloudsSprite} height="30vh" />
-            <Scene>
-                <AnimatePresence>{children}</AnimatePresence>
-            </Scene>
-        </ScrollerContainer>
-        <Ground>
-            <p>Samrith Shankar (c) {new Date().getFullYear()}.</p>
-        </Ground>
-    </Container>
-);
+export const Backdrop: React.FC = ({ children }) => {
+    const { scene } = useContext(GameContext);
+    const [scroll, setScroll] = useState(0);
+
+    return (
+        <Container>
+            <ScrollerContainer>
+                <Scroller src={CloudsSprite} height="30vh" width="100vw" />
+                <Scene id="scene">
+                    <SceneContent key="scene" animate={{ x: scroll }}>
+                        <AnimatePresence>{children}</AnimatePresence>
+                    </SceneContent>
+                    <SceneScrollHide />
+                </Scene>
+                <ScrollContext.Provider value={{ scroll, setScroll }}>
+                    <AnimatePresence>
+                        {scene !== SCENES.INTRO && <Player key="player" />}
+                    </AnimatePresence>
+                </ScrollContext.Provider>
+            </ScrollerContainer>
+            <Ground>
+                <p>Samrith Shankar (c) {new Date().getFullYear()}.</p>
+            </Ground>
+        </Container>
+    );
+};
