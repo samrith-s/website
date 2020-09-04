2c2abe4dee54e7ec8790d41abe326f8a55567df3
Samrith Shankar <samrith.298@gmail.com>
2020-09-05 00:40:43 +0200

    chore(scene): added different scene containers and fixed existing ones

--- styles/containers.ts
+++ styles/containers.ts
@@ -1,12 +1,38 @@
 import { motion } from 'framer-motion';
 import styled from 'styled-components';
 
+import { padding } from './helpers';
+
 export const SceneContainer = styled(motion.div)`
+    position: relative;
+    display: flex;
+    width: max-content;
+    height: 100%;
+`;
+
+export const PrimarySceneWrapper = styled(motion.div)`
     position: absolute;
-    top: 0;
+    top: 300px;
     right: 0;
-    bottom: 0;
+    bottom: 50px;
+    left: 0;
+`;
+
+export const SecondarySceneWrapper = styled(motion.div)`
+    position: absolute;
+    top: 300px;
+    right: 0;
+    bottom: 50px;
     left: 0;
     display: flex;
-    width: 100%;
+    width: max-content;
+    align-items: flex-end;
+
+    ${SceneContainer} {
+        ${padding(0, 15, 0, 0)}
+
+        &:first-of-type {
+            ${padding(0, 15, 0, 15)}
+        }
+    }
 `;
