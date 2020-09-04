c13071a458451f0011bbf153aba1f6b1714d9c07
Samrith Shankar <samrith.298@gmail.com>
2020-09-05 00:35:19 +0200

    chore(scene): added scene variables for Work

--- game/variables.ts
+++ game/variables.ts
@@ -1,6 +1,23 @@
 export enum SCENES {
     INTRO = 'intro',
     ABOUT = 'about',
+    WORK = 'work',
 }
 
-export type SCENE_VALUES = 'intro' | 'about';
+export type SCENE_VALUES = 'intro' | 'about' | 'work';
+
+export type SceneOrderType = {
+    [key in SCENE_VALUES]?: {
+        previous?: string;
+        next?: string;
+    };
+};
+
+export const SceneOrder: SceneOrderType = {
+    about: {
+        next: 'work',
+    },
+    work: {
+        previous: 'about',
+    },
+};
