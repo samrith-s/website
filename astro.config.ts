846c1c6616bc009cce2b0b9cdcb44340277b8e07
Samrith Shankar <samrith.298@gmail.com>
2024-12-05 00:40:52 +0530

    Upgrade to Astro v5 stable

--- astro.config.ts
+++ astro.config.ts
@@ -4,7 +4,7 @@ import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 import expressiveCode from "astro-expressive-code";
 
--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
 module.exports = {
   plugins: [require("autoprefixer")],
 };
